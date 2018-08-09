e5f5a15373d5a5e8492902fea04254a4b8d9cd4a
fuweiwei <fuweiwei@kjtpay.com.cn>
2018-08-09 10:50:25 +0800

    增加特效

--- plane_main.py
+++ plane_main.py
@@ -9,41 +9,30 @@ CREATE_ENEMY_EVENT = pygame.USEREVENT
 HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
-class PlaneGame(object):
+class PlaneGame:
     """飞机大战游戏类"""
 
     def __init__(self):
+        print("游戏初始化...")
 
-        # 1. pygame 初始化
         pygame.init()
 
-        # 2. 创建游戏屏幕
         self.screen = pygame.display.set_mode(SCREEN_RECT.size)
-
-        # 3. 创建游戏时钟
         self.clock = pygame.time.Clock()
 
-        # 4. 创建精灵组
         self.__create_sprites()
-
-        # 5. 创建用户事件
         PlaneGame.__create_user_events()
 
     def __create_sprites(self):
         """创建精灵组"""
 
-        # 背景组
-        bg1 = Background()
-        bg2 = Background(True)
-        self.back_group = pygame.sprite.Group(bg1, bg2)
+        self.back_group = pygame.sprite.Group(Background(), Background(True))
 
-        # 敌机组
-        enemy = Enemy()
-        self.enemy_group = pygame.sprite.Group(enemy)
-
-        # 英雄组
         self.hero = Hero()
         self.hero_group = pygame.sprite.Group(self.hero)
+
+        self.enemy_group = pygame.sprite.Group()
+        self.destroy_group = pygame.sprite.Group()
 
     @staticmethod
     def __create_user_events():
@@ -56,38 +45,60 @@ class PlaneGame(object):
         pygame.time.set_timer(HERO_FIRE_EVENT, 500)
 
     def start_game(self):
-        """开始游戏"""
+        """开启游戏循环"""
+
         while True:
-            # 1. 设置刷新帧率
             self.clock.tick(60)
-            # 2. 事件监听
             self.__event_handler()
-            # 3. 更新精灵组
             self.__update_sprites()
-            # 碰撞检测
             self.__check_collide()
-            # 4. 更新屏幕显示
             pygame.display.update()
 
+    def __check_collide(self):
+        """碰撞检测"""
+
+        # 子弹摧毁敌机
+        enemies = pygame.sprite.groupcollide(self.enemy_group,
+                                             self.hero.bullets,
+                                             False,
+                                             True).keys()
+        for enemy in enemies:
+            enemy.life -= 1
+
+            if enemy.life <= 0:
+                enemy.add(self.destroy_group)
+                enemy.remove(self.enemy_group)
+
+                enemy.destroied()
+
+        # 敌机撞毁英雄
+        for hero in pygame.sprite.spritecollide(self.hero,
+                                                self.enemy_group,
+                                                True):
+            print("英雄牺牲了...")
+            self.hero.destroied()
+
     def __event_handler(self):
-        """事件监听"""
+        """事件处理"""
 
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
-                print("退出游戏...")
-                pygame.quit()
-                exit()
-            elif event.type == CREATE_ENEMY_EVENT:
-                # 创建敌机，并且添加到敌机组
-                self.enemy_group.add(Enemy())
-
-                # 测试敌机精灵数量
-                # enemy_count = len(self.enemy_group.sprites())
-                # print("敌机精灵数量 %d" % enemy_count)
+                PlaneGame.__finished_game()
             elif event.type == HERO_FIRE_EVENT:
-                # 英雄发射子弹
                 self.hero.fire()
+            elif event.type == CREATE_ENEMY_EVENT:
+                self.enemy_group.add(Enemy())
+            # 按下 b 英雄自爆
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+                # self.hero.destroied()
+
+                # 集体自爆
+                for enemy in self.enemy_group.sprites():
+                    enemy.destroied()
+
+        # 判断英雄是否已经被销毁，如果是，游戏结束！
+        if self.hero.can_destroied:
+            PlaneGame.__finished_game()
 
         # 通过 pygame.key 获取用户按键
         keys_pressed = pygame.key.get_pressed()
@@ -97,34 +108,22 @@ class PlaneGame(object):
         self.hero.speed = dir * 2
 
     def __update_sprites(self):
-        """更新精灵组"""
+        """更新/绘制精灵组"""
 
-        for group in [self.back_group, self.enemy_group,
-                      self.hero_group, self.hero.bullets]:
-
+        for group in [self.back_group, self.hero_group,
+                      self.hero.bullets, self.enemy_group,
+                      self.destroy_group]:
             group.update()
             group.draw(self.screen)
 
-    def __check_collide(self):
-        """碰撞检测"""
+    @staticmethod
+    def __finished_game():
+        """退出游戏"""
 
-        # 1. 子弹摧毁敌机
-        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        print("退出游戏")
+        pygame.quit()
+        exit()
 
-        # 2. 英雄被撞毁
-        collide_list = pygame.sprite.spritecollide(self.hero, self.enemy_group, False)
-
-        if len(collide_list) > 0:
-            self.hero.is_alive = False
-
-            print("英雄牺牲...")
-
-            pygame.quit()
-            exit()
 
 if __name__ == '__main__':
-    # 1. 创建游戏对象
-    game = PlaneGame()
-
-    # 2. 开始游戏
-    game.start_game()
+    PlaneGame().start_game()
--- plane_sprites.py
+++ plane_sprites.py
@@ -1,86 +1,124 @@
 import random
 import pygame
 
-
-# 游戏屏幕大小
+# 屏幕尺寸
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 
 
 class GameSprite(pygame.sprite.Sprite):
-    """游戏精灵基类"""
+    """游戏精灵"""
 
     def __init__(self, image_name, speed=1):
-
-        # 调用父类的初始化方法
         super().__init__()
 
-        # 加载图像
         self.image = pygame.image.load(image_name)
-        # 设置尺寸
         self.rect = self.image.get_rect()
-        # 记录速度
         self.speed = speed
 
     def update(self, *args):
-
-        # 默认在垂直方向移动
         self.rect.top += self.speed
+
+    @staticmethod
+    def image_names(prefix, count):
+        names = []
+        for i in range(1, count + 1):
+            names.append("./images/" + prefix + str(i) + ".png")
+
+        return names
 
 
 class Background(GameSprite):
     """背景精灵"""
 
     def __init__(self, is_alt=False):
+        super().__init__("./images/background.png")
 
-        image_name = "./images/background.png"
-        super().__init__(image_name)
-
-        # 判断是否交替图片，如果是，将图片设置到屏幕顶部
         if is_alt:
             self.rect.bottom = 0
 
     def update(self, *args):
-
-        # 调用父类方法
         super().update(args)
 
-        # 判断是否超出屏幕
         if self.rect.top >= SCREEN_RECT.height:
             self.rect.bottom = 0
 
 
-class Enemy(GameSprite):
-    """敌机精灵"""
+class PlaneSprite(GameSprite):
+    """飞机精灵，包括敌机和英雄"""
 
-    def __init__(self):
+    def __init__(self, image_names, destroy_names, life, speed):
 
-        image_name = "./images/enemy1.png"
-        super().__init__(image_name)
+        image_name = image_names[0]
+        super().__init__(image_name, speed)
 
-        # 随机敌机出现位置
-        width = SCREEN_RECT.width - self.rect.width
-        self.rect.left = random.randint(0, width)
-        self.rect.bottom = 0
+        # 生命值
+        self.life = life
 
-        # 随机速度
-        self.speed = random.randint(1, 3)
+        # 正常图像列表
+        self.__life_images = []
+        for file_name in image_names:
+            image = pygame.image.load(file_name)
+            self.__life_images.append(image)
+
+        # 被摧毁图像列表
+        self.__destroy_images = []
+        for file_name in destroy_names:
+            image = pygame.image.load(file_name)
+            self.__destroy_images.append(image)
+
+        # 默认播放生存图片
+        self.images = self.__life_images
+        # 显示图像索引
+        self.show_image_index = 0
+        # 是否循环播放
+        self.is_loop_show = True
+        # 是否可以被删除
+        self.can_destroied = False
 
     def update(self, *args):
+        self.update_images()
+
         super().update(args)
 
-        # 判断敌机是否移出屏幕
-        if self.rect.top >= SCREEN_RECT.height:
-            # 将精灵从所有组中删除
-            self.kill()
+    def update_images(self):
+        """更新图像"""
+
+        pre_index = int(self.show_image_index)
+        self.show_image_index += 0.05
+        count = len(self.images)
+
+        # 判断是否循环播放
+        if self.is_loop_show:
+            self.show_image_index %= len(self.images)
+        elif self.show_image_index > count - 1:
+            self.show_image_index = count - 1
+            self.can_destroied = True
+
+        current_index = int(self.show_image_index)
+
+        if pre_index != current_index:
+            self.image = self.images[current_index]
+
+    def destroied(self):
+        """飞机被摧毁"""
+
+        # 默认播放生存图片
+        self.images = self.__destroy_images
+        # 显示图像索引
+        self.show_image_index = 0
+        # 是否循环播放
+        self.is_loop_show = False
 
 
-class Hero(GameSprite):
+class Hero(PlaneSprite):
     """英雄精灵"""
 
     def __init__(self):
 
-        image_name = "./images/me1.png"
-        super().__init__(image_name, 0)
+        image_names = GameSprite.image_names("me", 2)
+        destroy_names = GameSprite.image_names("me_destroy_", 4)
+
+        super().__init__(image_names, destroy_names, 0, 0)
 
         # 设置初始位置
         self.rect.centerx = SCREEN_RECT.centerx
@@ -90,6 +128,7 @@ class Hero(GameSprite):
         self.bullets = pygame.sprite.Group()
 
     def update(self, *args):
+        self.update_images()
 
         # 飞机水平移动
         self.rect.left += self.speed
@@ -101,6 +140,7 @@ class Hero(GameSprite):
             self.rect.right = SCREEN_RECT.right
 
     def fire(self):
+        """发射子弹"""
 
         # bullet_count = len(self.bullets.sprites())
         # print("子弹数量 %d" % bullet_count)
@@ -121,14 +161,41 @@ class Bullet(GameSprite):
     """子弹精灵"""
 
     def __init__(self):
-
         image_name = "./images/bullet1.png"
         super().__init__(image_name, -2)
 
     def update(self, *args):
-
         super().update(args)
 
         # 判断是否超出屏幕
         if self.rect.bottom < 0:
+            self.kill()
+
+
+class Enemy(PlaneSprite):
+    """敌机精灵"""
+
+    def __init__(self):
+        image_names = ["./images/enemy1.png"]
+        destroy_names = GameSprite.image_names("enemy1_down", 4)
+        super().__init__(image_names, destroy_names, 2, 1)
+
+        # 随机敌机出现位置
+        width = SCREEN_RECT.width - self.rect.width
+        self.rect.left = random.randint(0, width)
+        self.rect.bottom = 0
+
+        # 随机速度
+        self.speed = random.randint(1, 3)
+
+    def update(self, *args):
+        super().update(args)
+
+        # 判断敌机是否移出屏幕
+        if self.rect.top >= SCREEN_RECT.height:
+            # 将精灵从所有组中删除
+            self.kill()
+
+        # 判断敌机是否已经被销毁
+        if self.can_destroied:
             self.kill()
